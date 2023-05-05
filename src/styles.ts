2e32d9d5f768671f9512acd4749bc8423a55ccd3
LuizFernandoDeveloper <luiz.fernando.developer@outlook.com>
2023-05-05 17:37:28 -0300

    initialsetup

---
+++ src/styles.ts
@@ -0,0 +1,9 @@
+import { createGlobalStyle } from 'styled-components'
+
+const EstiloGlobalComJs = createGlobalStyle`
+  *{
+    margin: 0 !important;
+    padding:0 !importate;
+  }
+`
+export default EstiloGlobalComJs
